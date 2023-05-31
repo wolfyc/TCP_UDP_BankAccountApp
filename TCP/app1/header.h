b11a5095fdd1674f0cefa57c9582100f73e9082c
wolfycc <zaidimedh@gmail.com>
2023-05-31 15:18:32 +0200

    header_finished

--- TCP/app1/header.h
+++ TCP/app1/header.h
@@ -5,18 +5,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <netinet/in.h>
 
-#define port 8080
+#define MAXPENDING 5    /* Max connection requests */
+#define BUFFSIZE 1024
+#define port 8081
 
-#define BUFFER_SIZE 1024
-
-Account account_db[] = {
-    // {id_client, id_compte, password, solde, {{OPERATIONS}}, nombre_operations}
-    {1, 1001, "password1", 5000.0, {{"", "", 0}}, 0},
-    {2, 1002, "password2", 3000.0, {{"", "", 0}}, 0},
-    {3, 0000, "p", 3999.0, {{"", "", 0}}, 0}
-};
-int account_db_nbr = sizeof(account_db) / sizeof(account_db[0]);
 
 typedef struct operations_ {
     char type_operation[10];
@@ -26,12 +23,19 @@ typedef struct operations_ {
 
 typedef struct account_{
     int id_client;
-    int id_Account;
+    int id_account;
     char password[32];
     double balance;
     Operations operations[11];
     int Operation_nbr;
 } Account;
+Account account_db[] = {
+    // {id_client, id_compte, password, solde, {{OPERATIONS}}, nombre_operations}
+    {1, 1001, "password1", 5000.0, {{"", "", 0}}, 0},
+    {2, 1002, "password2", 3000.0, {{"", "", 0}}, 0},
+    {3, 0000, "p", 3999.0, {{"", "", 0}}, 0}
+};
+int account_db_nbr = sizeof(account_db) / sizeof(account_db[0]);
 
 void save_operation(Account *Account, const char *type_operation, double amount);
 Account* find_accound_by_ID(int id_client, int id_Account, const char *password);
@@ -39,15 +43,9 @@ int AJOUT(int id_client, int id_Account, const char *password, double somme);
 int RETRAIT(int id_client, int id_Account, const char *password, double somme);
 double SOLDE(int id_client, int id_Account, const char *password);
 char *OPERATIONS(int id_client, int id_Account, const char *password, char *buffer, size_t buffer_size);
+void Die(char *mess);
+void HandleClient(int sock);
 
-void save_operation(Account *Account, const char *type_operation, double amount) {
-    Operations *operation = &Account->operations[Account->Operation_nbr % 10];
-    strcpy(operation->type_operation, type_operation);
-    time_t t = time(NULL);
-    strftime(operation->date_operation, sizeof(operation->date_operation), "%Y-%m-%d %H:%M:%S", localtime(&t));
-    operation->operation_amount = amount;
-    Account->Operation_nbr++;
-}
 void save_operation(Account* account, const char* type_operation, double amount) {
     Operations* operation = &(account->operations[account->Operation_nbr % 10]);
     strncpy(operation->type_operation, type_operation, sizeof(operation->type_operation) - 1); // avoid overflow
@@ -60,8 +58,8 @@ void save_operation(Account* account, const char* type_operation, double amount)
 Account* find_account_by_ID(int id_client, int id_account, const char* password) {
     for (int i = 0; i < account_db_nbr; i++) {
         if (account_db[i].id_client == id_client && account_db[i].id_account == id_account &&
-            strcmp(account_db[i].password, password) == 0) {
-            return &account_db[i];
+    strcmp(account_db[i].password, password) == 0) {
+    return &account_db[i];
         }
     }
     return NULL;
@@ -87,5 +85,59 @@ int RETRAIT(int id_client, int id_account, const char* password, double amount) 
     }
     return 0;
 }
+
+double SOLDE(int id_client, int id_account, const char* password) {
+    Account* account = find_account_by_ID(id_client, id_account, password);
+    if (account) {
+        return account->balance;
+    }
+    return -1.0;
+}
+
+char* OPERATIONS(int id_client, int id_account, const char* password, char* buffer, size_t buffer_size) {
+    Account* account = find_account_by_ID(id_client, id_account, password);
+    if (account) {
+        int start = (account->Operation_nbr > 10) ? account->Operation_nbr - 10 : 0;
+        buffer[0] = '\0';
+        for (int i = start; i < account->Operation_nbr; i++) {
+            Operations* operation = &(account->operations[i % 10]);
+            char line[128];
+            snprintf(line, sizeof(line), "%s %s %.3lf\n", operation->type_operation, operation->date_operation, operation->operation_amount);
+            strncat(buffer, line, buffer_size - strlen(buffer) - 1);
+        }
+        return buffer;
+    }
+    return NULL;
+}
+void HandleClient(int sock) {
+    char buffer[BUFFSIZE];
+    int received = -1;
+    char *message_client = "\nServer status READY";
+    /* Receive message */
+    if ((received = recv(sock, buffer, BUFFSIZE, 0)) < 0) {
+      Die("Failed to receive initial bytes from client");
+    }
+    printf("buffer Message received is: %s\n", buffer);
+    /* Send bytes and check for more incoming data in loop */
+    while(received > 0){
+      /* Send back received data */
+      if (send(sock, buffer, received, 0) != received) {
+        Die("Failed to send bytes to client");
+      }
+      buffer[received] = '\0';
+      //send(sock,message_client, strlen(message_client), 0);
+      printf("\nMessage sent back is : %s\n", buffer);
+      /* Check for more data */
+      if ((received = recv(sock, buffer, BUFFSIZE, 0)) < 0) {
+        Die("Failed to receive additional bytes from client");
+      }
+    }
+        close(sock);
+    }
+    void Die(char *mess)
+    {
+        perror(mess);
+        exit(1);
+    }
 
 #endif // SERVER_H
--- TCP/app1/server_tcp.c
+++ TCP/app1/server_tcp.c
@@ -1,77 +1,43 @@
+#include "header.h"
 
-            #include <stdio.h>
-            #include <sys/socket.h>
-            #include <arpa/inet.h>
-            #include <stdlib.h>
-            #include <string.h>
-            #include <unistd.h>
-            #include <netinet/in.h>
+int main(int argc, char *argv[])
+{
+  int serversock, clientsock;
+  struct sockaddr_in echoserver, echoclient;
+  /*
 
-            #include "header.h"
-
-          #define MAXPENDING 5    /* Max connection requests */
-          #define BUFFSIZE 1024
-          #define port 8081
-          void Die(char *mess) { perror(mess); exit(1); }
-          
-          
-          void HandleClient(int sock) {
-            char buffer[BUFFSIZE];
-            int received = -1;
-            char *message_client = "\nServer status READY";
-            /* Receive message */
-            if ((received = recv(sock, buffer, BUFFSIZE, 0)) < 0) {
-              Die("Failed to receive initial bytes from client");
-            }
-            printf("buffer Message received is: %s\n", buffer);
-            /* Send bytes and check for more incoming data in loop */
-         while(received > 0){
-              /* Send back received data */
-              if (send(sock, buffer, received, 0) != received) {
-                Die("Failed to send bytes to client");
-              }
-              buffer[received] = '\0';
-              //send(sock,message_client, strlen(message_client), 0);
-              printf("\nMessage sent back is : %s\n", buffer);
-              /* Check for more data */
-              if ((received = recv(sock, buffer, BUFFSIZE, 0)) < 0) {
-                Die("Failed to receive additional bytes from client");
-              }
-            }
-            close(sock);
-          }
-
-          int main(int argc, char *argv[]) {
-            int serversock, clientsock;
-            struct sockaddr_in echoserver, echoclient;
-/*
-           
-            /* Create the TCP socket */
-            if ((serversock = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0) {
-              Die("Failed to create socket");
-            }
-            /* Construct the server sockaddr_in structure */
-            memset(&echoserver, 0, sizeof(echoserver));       /* Clear struct */
-            echoserver.sin_family = AF_INET;                  /* Internet/IP */
-            echoserver.sin_addr.s_addr = INADDR_ANY;//  /* Incoming addr */
-            echoserver.sin_port = htons(port);       /* server port */
-            /* Bind the server socket */
-            if (bind(serversock, (struct sockaddr *) &echoserver,
-                                        sizeof(echoserver)) < 0) {
-                Die("Failed to bind the server socket");
-            }
-            /* Listen on the server socket */
-            if (listen(serversock, MAXPENDING) < 0) {
-                Die("Failed to listen on server socket");
-            }
-            /* Run until cancelled */
-            while (1) {
-              unsigned int clientlen = sizeof(echoclient);
-              /* Wait for client connection */
-              if ((clientsock = accept(serversock, (struct sockaddr *) &echoclient,&clientlen)) < 0) {
-                Die("Failed to accept client connection");
-              }
-              fprintf(stdout, "Client connected: %s\nWaiting for instructions from client\n",inet_ntoa(echoclient.sin_addr));
-              HandleClient(clientsock);
-            }
-        }
+              /* Create the TCP socket */
+  if ((serversock = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0)
+  {
+    Die("Failed to create socket");
+  }
+  /* Construct the server sockaddr_in structure */
+  memset(&echoserver, 0, sizeof(echoserver)); /* Clear struct */
+  echoserver.sin_family = AF_INET;            /* Internet/IP */
+  echoserver.sin_addr.s_addr = INADDR_ANY;    //  /* Incoming addr */
+  echoserver.sin_port = htons(port);          /* server port */
+  /* Bind the server socket */
+  if (bind(serversock, (struct sockaddr *)&echoserver,
+           sizeof(echoserver)) < 0)
+  {
+    Die("Failed to bind the server socket");
+  }
+  /* Listen on the server socket */
+  if (listen(serversock, MAXPENDING) < 0)
+  {
+    Die("Failed to listen on server socket");
+  }
+  unsigned int clientlen = sizeof(echoclient);
+  /* Run until cancelled */
+  while (1)
+  {
+    
+    /* Wait for client connection */
+    if ((clientsock = accept(serversock, (struct sockaddr *)&echoclient, &clientlen)) < 0)
+    {
+      Die("Failed to accept client connection");
+    }
+    fprintf(stdout, "Client connected: %s\nWaiting for instructions from client\n", inet_ntoa(echoclient.sin_addr));
+    //HandleClient(clientsock);
+  }
+}
